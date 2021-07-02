09382942badee190fb88b5b40313eba13693b361
RakibRahman <md.rr.talukder@gmail.com>
2021-07-02 22:15:40 +0600

    typo fixed

--- js/text.js
+++ js/text.js
@@ -4,7 +4,7 @@ const Text = ((_) => {
     "To know sorrow is not terrifying. What is terrifying is to know you can't go back to happiness you could have.",
 
     "Whatever you lose, you'll find it again. But what you throw away you'll never get back.",
-    "If you don't like your destiny, don’t accept it. Instead, have the courage to change it the way you want it to be.",
+    "If you don't like your destiny, don't accept it. Instead, have the courage to change it the way you want it to be.",
 
     "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
 
@@ -14,15 +14,15 @@ const Text = ((_) => {
 
     "Religion, ideology, resources, land, spite, love or just because… No matter how pathetic the reason, it's enough to start war. War will never cease to exist… reasons can be thought up after the fact… Human nature pursues strife.",
 
-    "Thinking you're no-good and worthless is the worst thing you can do",
+    "Thinking you're no-good and worthless is the worst thing you can do.",
     "Don't give up, there’s no shame in falling down! True shame is to not stand up again!",
     "People's lives don't end when they die. It ends when they lose faith.",
     "Life is not a game of luck. If you wanna win, work hard.",
-    "Because people don't have wings, the look for ways to fly.",
+    "Because people don't have wings, they look for ways to fly.",
     " Do you think that someday, the real stars will ever come back?",
     "Sometimes you must hurt in order to know, fall in order to grow, lose in order to gain.",
   ];
-  console.log(para.length);
+
   const randomPara = (_) => {
     const pick = Math.floor(Math.random() * para.length);
     return para[pick];
